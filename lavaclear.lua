12c2b778612e2425b5c077567f4b0df0f08d3bfc
public-account-7 <doitenroimain@gmail.com>
2025-04-13 03:36:00 +0700

    Update lavaclear.lua

--- lavaclear.lua
+++ lavaclear.lua
@@ -4,17 +4,12 @@ if game.PlaceId == 15552588346 then
 
     function bindable.OnInvoke(buttonPressed)
         if buttonPressed == "Yes" then
-            if workspace:FindFirstChild("Lava") then
-                workspace:FindFirstChild("Lava"):Destroy()
-                print("lava cooked lmao")
-            end
-
-            workspace.DescendantAdded:Connect(function(child)
-                if child.Name == "Lava" then
-                    child:Destroy()
+            while task.wait(1) do
+                if workspace:FindFirstChild("Lava") then
+                    workspace:FindFirstChild("Lava"):Destroy()
                     print("lava cooked lmao")
                 end
-            end)
+            end
         end
     end
 
